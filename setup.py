f02a35b40cf0d342e47c18b2b22b5b05c4ee4c49
Dylan Stephano-Shachter <dstephanoshachter@gmail.com>
2019-01-19 17:22:32 -0500

    packaging improvements

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ import os
 from setuptools import setup
 
 setup(
-    name = 'fastprocess',
-    version = '0.1.0',
-    author = 'Dylan Stephano-Shachter',
-    author_email = 'dstephanoshachter@gmail.com',
-    description = ('A fast subprocess library'),
-    license = 'LGPL',
-    url = 'https://github.com/dstathis/fastprocess',
-    packages = ['fastprocess'],
-    long_description = open(
+    name='fastprocess',
+    version='0.1.0',
+    author='Dylan Stephano-Shachter',
+    author_email='dstephanoshachter@gmail.com',
+    description=('A fast subprocess library'),
+    license='LGPL',
+    url='https://github.com/dstathis/fastprocess',
+    packages=['fastprocess'],
+    long_description=open(
         os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
-    ).read()
+    ).read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
