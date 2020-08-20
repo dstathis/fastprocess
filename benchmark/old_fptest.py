a42c8a8bd78b4a1c3bfcc3e19b1709652dd5c75d
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2020-08-20 01:27:30 -0400

    remove OldFastProcess

--- benchmark/old_fptest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from fastprocess import FastProcess, OldFastProcess
-
-procs = []
-
-for i in range(10000):
-    procs.append(OldFastProcess(['echo', 'hello', 'world'], stdout=open('/dev/null', 'w')))
--- fastprocess/fastprocess.py
+++ fastprocess/fastprocess.py
@@ -26,28 +26,3 @@ class FastProcess:
     def wait(self):
         self.waited = True
         return os.WEXITSTATUS(os.waitpid(self.pid, 0)[1])
-
-
-class OldFastProcess:
-    def __init__(self, cmd, stdin=None, stdout=None, stderr=None):
-        self.pid = os.fork()
-        if self.pid == 0:
-            if stdin:
-                os.dup2(stdin.fileno(), 0)
-            if stdout:
-                os.dup2(stdout.fileno(), 1)
-            if stderr:
-                os.dup2(stderr.fileno(), 2)
-            os.execvp(cmd[0], cmd)
-
-    def __del__(self):
-        os.kill(self.pid, signal.SIGTERM)
-
-    def terminate(self):
-        os.kill(self.pid, signal.SIGTERM)
-
-    def signal(self, sig):
-        os.kill(self.pid, sig)
-
-    def wait(self):
-        return os.WEXITSTATUS(os.waitpid(self.pid, 0)[1])
